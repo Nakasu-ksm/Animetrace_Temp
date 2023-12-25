7e8ee1580e4e5b353c36bd6acd08f85366faf6ab
稲森光香 <42112059+itoukou1@users.noreply.github.com>
2023-12-25 16:55:33 +0900

    Create hm.js

---
+++ static/hm.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?62fe3ae605e6cb2c287f7a3f91c7e125";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
